2a1c6fe4d5882fd080dbbc6af1f068360b4d5552
Tatiana Sanchez <tatiana.sanchez.sanin@gmail.com>
2024-04-11 17:02:03 -0500

    passed tests 1 to 6

---
+++ enunciado/util.c
@@ -0,0 +1,60 @@
+#include "util.h"
+
+void del_newline(char *str)
+{
+    int length = strlen(str);
+    str[length - 1] = '\0';
+}
+
+char **split_array(char *str)
+{
+    if (str == NULL)
+    {
+        return NULL;
+    }
+    char **ret = malloc(100);
+    char *temp_string = "";
+    int i = 0;
+    int temp_counter = 0;
+    while (1)
+    {
+        temp_string = strsep(&str, " ");
+        if (temp_string == NULL)
+            break;
+        if (strcmp(temp_string, "") == 0) // Ignore whitespace "args"
+            continue;
+        ret[i] = temp_string;
+        i++;
+    }
+    ret[i] = '\0';
+    return ret;
+}
+
+char *arr_to_str(char *command, char **args)
+{
+    char *ret = malloc(100);
+    strcpy(ret, command);
+    int i = 0;
+    while (args[i] != 0)
+    {
+        strcat(ret, " ");
+        strcat(ret, args[i]);
+        i++;
+    }
+    strcat(ret, "\n\0");
+    return ret;
+}
+
+// For testing purposes
+void print_char_array(char **arr)
+{
+    for (int i = 0; i < MAX_ARGS; i++)
+    {
+        if (arr[i] != NULL)
+        {
+            printf(">>> array[%d] = %s\n", i, arr[i]);
+        }
+        else
+            break;
+    }
+}
